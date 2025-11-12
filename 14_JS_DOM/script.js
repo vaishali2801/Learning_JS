bb0cbea1a64729321fe45df830ade23762782223
vaishali2801 <vaishalichauhan2801@gmail.com>
2025-11-12 19:15:57 +0530

    DOM

---
+++ 14_JS_DOM/script.js
@@ -0,0 +1,8 @@
+//DOM (Document Object Model) is a tree-like structure that represents your HTML document.
+// using id only
+document.getElementById("para").textContent = "para change using script";
+
+//using class only
+document.getElementsByClassName("class")[0].innerHTML = "<h1>This is using class</h1>";
+
+document.getElementById("btn").innerHTML = "<h1>log out</h1>"
