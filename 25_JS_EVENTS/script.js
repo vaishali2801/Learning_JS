1596096d864fcd004a0fb533a2b3ca4b4e9bcaef
vaishali2801 <vaishalichauhan2801@gmail.com>
2025-12-26 11:36:22 +0530

    events

---
+++ 25_JS_EVENTS/script.js
@@ -0,0 +1,54 @@
+
+// click event
+// $(document).ready(() => {
+//     $("#btn").click(() => {
+//         $("#img").css({ 
+//             width: "600px",
+//             height : "500px"
+//         });
+//     });
+// });
+
+//double click event
+$(document).ready(() => {
+    $("#btn").dblclick(() => {
+        $("#img").css({ 
+            width: "600px",
+            height : "450px"
+        });
+    });
+});
+
+//mouseenter event
+// $(document).ready(() => {
+//     $("#btn").mouseenter(() => {
+//         $("#img").css({ 
+//             width: "400px",
+//             height : "300px"
+//         });
+//     });
+// });
+
+//mouseleave event
+// $(document).ready(() => {
+//     $("#btn").mouseleave(() => {
+//         $("#img").css({ 
+//             width: "500px",
+//             height : "500px"
+//         });
+//     });
+// });
+
+//key events
+// $(document).ready(() => {
+//     $(document).keyup(() => {
+//         console.log("key up....")
+//     });
+// });
+
+// $(document).ready(() => {
+//     $(document).keydown(() => {
+//         console.log("key down....")
+//     });
+// });
+
